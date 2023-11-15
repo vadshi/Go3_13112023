d128f88df0f35827130fd2a204ec952eaaa249d5
Vadim Shikhovtsov <vadimx30@gmail.com>
2023-11-15 11:06:12 +0300

    Add Entry and Transfers CRUD

--- bankstore/db/query/entry.sql
+++ bankstore/db/query/entry.sql
@@ -5,3 +5,25 @@ INSERT INTO entries (
 ) VALUES (
     $1, $2
 ) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries 
+WHERE id = $1 
+LIMIT 1;
+
+-- name: ListAEntries :many
+SELECT * FROM entries
+WHERE account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: UpdateEntry :one
+UPDATE entries
+SET amount = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteEntry :exec
+DELETE FROM entries
+WHERE id=$1;
--- bankstore/db/sqlc/entry.sql.go
+++ bankstore/db/sqlc/entry.sql.go
@@ -34,3 +34,94 @@ func (q *Queries) CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry
 	)
 	return i, err
 }
+
+const deleteEntry = `-- name: DeleteEntry :exec
+DELETE FROM entries
+WHERE id=$1
+`
+
+func (q *Queries) DeleteEntry(ctx context.Context, id int64) error {
+	_, err := q.db.Exec(ctx, deleteEntry, id)
+	return err
+}
+
+const getEntry = `-- name: GetEntry :one
+SELECT id, account_id, amount, created_at FROM entries 
+WHERE id = $1 
+LIMIT 1
+`
+
+func (q *Queries) GetEntry(ctx context.Context, id int64) (Entry, error) {
+	row := q.db.QueryRow(ctx, getEntry, id)
+	var i Entry
+	err := row.Scan(
+		&i.ID,
+		&i.AccountID,
+		&i.Amount,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
+const listAEntries = `-- name: ListAEntries :many
+SELECT id, account_id, amount, created_at FROM entries
+WHERE account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3
+`
+
+type ListAEntriesParams struct {
+	AccountID int64 `json:"account_id"`
+	Limit     int32 `json:"limit"`
+	Offset    int32 `json:"offset"`
+}
+
+func (q *Queries) ListAEntries(ctx context.Context, arg ListAEntriesParams) ([]Entry, error) {
+	rows, err := q.db.Query(ctx, listAEntries, arg.AccountID, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Entry
+	for rows.Next() {
+		var i Entry
+		if err := rows.Scan(
+			&i.ID,
+			&i.AccountID,
+			&i.Amount,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const updateEntry = `-- name: UpdateEntry :one
+UPDATE entries
+SET amount = $2
+WHERE id = $1
+RETURNING id, account_id, amount, created_at
+`
+
+type UpdateEntryParams struct {
+	ID     int64 `json:"id"`
+	Amount int64 `json:"amount"`
+}
+
+func (q *Queries) UpdateEntry(ctx context.Context, arg UpdateEntryParams) (Entry, error) {
+	row := q.db.QueryRow(ctx, updateEntry, arg.ID, arg.Amount)
+	var i Entry
+	err := row.Scan(
+		&i.ID,
+		&i.AccountID,
+		&i.Amount,
+		&i.CreatedAt,
+	)
+	return i, err
+}
