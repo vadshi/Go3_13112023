a9aa034afe2051231fb91021b6ebdb08fbc11437
Vadim Shikhovtsov <vadimx30@gmail.com>
2023-11-16 16:20:58 +0300

    Fix getAccount and add json collection

---
+++ bankstore/api/account.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	db "github.com/vadshi/go3/bankstore/db/sqlc"
+)
+
+type CreateAccountRequest struct {
+	Owner    string      `json:"owner" binding:"required"`
+	Currency db.Currency `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req CreateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct{
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == pgx.ErrNoRows{
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+// errorResponse return gin.H -> map[string]interface{}
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- bankstore/api/server.go
+++ bankstore/api/server.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	db "github.com/vadshi/go3/bankstore/db/sqlc"
 )
@@ -17,43 +15,13 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 
 	// TODO: add routes to router
-	router.POST("/accounts", server.CreateAccount)
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
 
 	server.router = router
 	return server
 }
 
-type CreateAccountRequest struct {
-	Owner    string      `json:"owner" binding:"required"`
-	Currency db.Currency `json:"currency" binding:"required,oneof=USD EUR"`
-}
-
-func (server *Server) CreateAccount(ctx *gin.Context) {
-	var req CreateAccountRequest
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return 
-	}
-
-	arg := db.CreateAccountParams{
-		Owner: req.Owner,
-		Currency: req.Currency,
-		Balance: 0,
-	}
-
-	account, err := server.store.CreateAccount(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return 
-	}
-	ctx.JSON(http.StatusOK, account)
-}
-
-// errorResponse return gin.H -> map[string]interface{}
-func errorResponse(err error) gin.H{
-	return gin.H{"error": err.Error()}
-}
-
-func (server *Server) Start(address string) error{
+func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
